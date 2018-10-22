1737edf66017dd4d9d0ce39385645bd6bafe35ad
rochellev <rochellevroberts@utexas.edu>
2018-10-22 13:23:46 -0700

    first commit

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,27 @@
 import os
 import webapp2
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
+	
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template,**kw))
 		
 class MainPage(Handler):
 	def get(self):
-		self.write("hello buddy, this is Pup")
+		n = self.request.get("n")
+		if n:
+			n = int(n)
+		self.render("shopping_list.html", n=n)
 
 app = webapp2.WSGIApplication([('/', MainPage),
 								], 
